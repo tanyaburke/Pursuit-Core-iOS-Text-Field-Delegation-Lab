2041c0dc82e19ca4b4a292c03a40fb4345e1820d
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-04 19:36:15 -0500

    initial commit

---
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextTwistProject
+//
+//  Created by Benjamin Stone on 11/4/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
